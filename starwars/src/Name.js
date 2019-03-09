c8baceb4d20358e43032b2b0373d439c50a8edb7
Christopher Harrison <cnharrison@gmail.com>
2019-03-09 13:19:35 -0500

    copyedit space before cm and kg

--- starwars/src/Name.js
+++ starwars/src/Name.js
@@ -8,8 +8,8 @@ function Name(props) {
         <li><span className="label">Gender:</span> {props.character.gender}</li>
         <li><span className="label">Age at Battle of Yavin:</span> {props.character.birth_year.replace('BBY','')}</li>
         <li><span className="label">Hair color:</span> {props.character.hair_color}</li>
-        <li><span className="label">Height:</span> {props.character.height}cm</li>
-        <li><span className="label">Weight:</span> {props.character.mass}kg</li>
+        <li><span className="label">Height:</span> {props.character.height} cm</li>
+        <li><span className="label">Weight:</span> {props.character.mass} kg</li>
       </ul>
     </div>
   );
